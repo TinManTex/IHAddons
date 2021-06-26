491a13c4202c218ea9b191d9f3224df9ca3e8bcf
TinManTex <tinman.squad@gmail.com>
2021-06-26 17:45:52 +1200

    Create afc0_routeSets.lua

---
+++ tpp/mgo3-map-afc0/Assets/mgo/pack/location/afc0/pack_common/afc0_script_fpkd/Assets/mgo/script/location/afc0/afc0_routeSets.lua
@@ -0,0 +1,64 @@
+local afc0_routeSets
+
+afc0_routeSets = {
+	--TODO: addo other cp common routes once they are solidified
+
+	quest_cp = {
+		priority = {
+			"groupA",
+		},
+		sneak_day = {
+			groupA = {
+				"rt_quest_dummy_0000",
+				"rt_quest_dummy_0000",
+				"rt_quest_dummy_0000",
+				"rt_quest_dummy_0000",
+				"rt_quest_dummy_0000",
+				"rt_quest_dummy_0000",
+				"rt_quest_dummy_0000",
+				"rt_quest_dummy_0000",
+			},
+		},
+		sneak_night = {
+			groupA = {
+				"rt_quest_dummy_0000",
+				"rt_quest_dummy_0000",
+				"rt_quest_dummy_0000",
+				"rt_quest_dummy_0000",
+				"rt_quest_dummy_0000",
+				"rt_quest_dummy_0000",
+				"rt_quest_dummy_0000",
+				"rt_quest_dummy_0000",
+			},
+		},
+		caution = {
+			groupA = {
+				"rt_quest_dummy_0000",
+				"rt_quest_dummy_0000",
+				"rt_quest_dummy_0000",
+				"rt_quest_dummy_0000",
+				"rt_quest_dummy_0000",
+				"rt_quest_dummy_0000",
+				"rt_quest_dummy_0000",
+				"rt_quest_dummy_0000",
+			},
+		},
+		hold = {
+			default = {
+			},
+		},
+		sleep = {
+			default = {
+			},
+		},
+		travel = {
+			lrrpHold = {
+			},
+		},
+		nil
+	},--quest_cp
+
+	nil
+}--afc0_routeSets
+
+return afc0_routeSets
