7cba9adfe3008d7f70c612579d16a914670f8d14
TinManTex <tinman.squad@gmail.com>
2021-06-26 17:45:48 +1200

    Create afc0_combat.lua

---
+++ tpp/mgo3-map-afc0/Assets/mgo/pack/location/afc0/pack_common/afc0_script_fpkd/Assets/mgo/script/location/afc0/afc0_combat.lua
@@ -0,0 +1,14 @@
+local afc0_combat
+
+afc0_combat={
+	afc0_briefing_cp={
+		"TppGuardTargetData0000",
+		"TppCombatLocatorSetData0000",
+	},
+	afc0_village_cp={
+		--"TppGuardTargetData0001",
+		"TppCombatLocatorSetData0001",
+	},
+}--afc0_combat
+
+return afc0_combat
