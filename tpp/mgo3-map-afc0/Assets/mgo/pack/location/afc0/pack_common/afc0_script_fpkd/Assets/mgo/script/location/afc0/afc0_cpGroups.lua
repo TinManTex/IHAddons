3bf4126acf8f23c84f151fb0d5fd14c6c32f2148
TinManTex <tinman.squad@gmail.com>
2021-07-02 09:57:56 +1200

    Create afc0_cpGroups.lua

---
+++ tpp/mgo3-map-afc0/Assets/mgo/pack/location/afc0/pack_common/afc0_script_fpkd/Assets/mgo/script/location/afc0/afc0_cpGroups.lua
@@ -0,0 +1,10 @@
+local afc0_cpGroups
+afc0_cpGroups={	
+	group_Area1={
+		afc0_briefing_cp=true,
+		afc0_village_cp=true,
+		--afc0_01_02_lrrp=true,
+		--afc0_02_01_lrrp=true,
+	},
+}
+return afc0_cpGroups
