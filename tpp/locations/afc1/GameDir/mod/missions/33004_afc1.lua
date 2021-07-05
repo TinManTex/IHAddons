524d6b545539de483bf2202a99f8b7c58ce8a67f
TinManTex <tinman.squad@gmail.com>
2021-07-06 09:32:19 +1200

    afc1

---
+++ tpp/locations/afc1/GameDir/mod/missions/33004_afc1.lua
@@ -0,0 +1,12 @@
+local this={
+  description="Amber Station Free Roam",
+  missionCode=33004,
+  location="AFC1",
+  packs={
+    "/Assets/tpp/pack/mission2/ih/quest_block.fpk",
+    "/Assets/tpp/pack/mission2/free/fafc1/fafc1_mission.fpk",
+  },
+  startPos={3.847,-5.105,79.635},--rotY=159.648,},
+}
+
+return this
