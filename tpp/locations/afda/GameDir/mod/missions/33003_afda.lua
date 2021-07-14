b5ec7d57b7924bff0270ac2195c0e1a935c883e4
TinManTex <tinman.squad@gmail.com>
2021-07-15 09:57:49 +1200

    afda

---
+++ tpp/locations/afda/GameDir/mod/missions/33003_afda.lua
@@ -0,0 +1,11 @@
+local this={
+  missionCode=33003,
+  location="AFDA",
+  packs={
+    "/Assets/tpp/pack/mission2/ih/quest_block.fpk",
+    "/Assets/tpp/pack/mission2/free/fafda/fafda_mission.fpk",
+  },
+  startPos={-22.625,19.727,122.175},--rotY=-174.437,},
+}
+
+return this
