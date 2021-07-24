a56da90fed1cdaf61790c6615ee38dfc51ad6f92
TinManTex <tinman.squad@gmail.com>
2021-07-25 08:59:39 +1200

    land point

--- tpp/locations/afc0/GameDir/mod/missions/33001_afc0.lua
+++ tpp/locations/afc0/GameDir/mod/missions/33001_afc0.lua
@@ -2,8 +2,9 @@ local this={
   description="Jade Forest Free Roam",
   missionCode=33001,
   location="AFC0",  
-  startPos={-71.862,0.584,248.143},--rotY=143.701,},--bottom of map by river
+  startPos={-71.862,0.584,248.143},--rotY=143.701,},--south by river
   --startPos={-11.788,8.483,165.559},--rotY=-30.511,},--mgo title pos next to radio
+  --{pos={-57.259,2.371,-204.524},rotY=-15.031,},--north behind shack
   packs=function(missionCode)
     TppPackList.AddMissionPack"/Assets/mgo/pack/location/afc0/pack_common/afc0_script.fpk"--REF TppPackList.AddLocationCommonScriptPack(missionCode), not set up for addon locations. even though mgo location doesnt have common script pack keeping it in there just to be consistant with the other location packs
     --REF TppPackList.AddLocationCommonMissionAreaPack(missionCode)-- not set up for addon locations --> 
@@ -17,10 +18,10 @@ local this={
     TppPackList.AddMissionPack"/Assets/tpp/pack/mission2/ih/ih_soldier_base.fpk"--tex mis_com_mafr isn't actually a complete enemy pack, normal missions roll these files into the mission packs
     TppPackList.AddMissionPack"/Assets/tpp/pack/mission2/common/mis_com_mafr.fpk"--soldier pack
     TppPackList.AddMissionPack"/Assets/tpp/pack/mission2/ih/snd_ene_af.fpk"--EnemyType.TYPE_PF, CpType.TYPE_AFRIKAANS
-    --TppPackList.AddMissionPack"/Assets/tpp/pack/mission2/ih/subs_boot_ene_af.fpk"-- f30020_subtitles -> <lang>Voice/<lang>Text/ene_common_af.subp,_str.subp DEBUGNOW off till IH release
+    TppPackList.AddMissionPack"/Assets/tpp/pack/mission2/ih/subs_boot_ene_af.fpk"-- f30020_subtitles -> <lang>Voice/<lang>Text/ene_common_af.subp,_str.subp
     TppPackList.AddMissionPack"/Assets/tpp/pack/mission2/ih/quest_block.fpk"--DEBUGNOW test if solder locators work here or if they need to be after Soldier2GameObject in _mission pack
     TppPackList.AddMissionPack"/Assets/tpp/pack/mission2/free/fafc0/fafc0_mission.fpk"--tex deviating from the norm a bit and naming it after location rather than free missioncode, also vanilla free mission packs dont have _mission suffix, story have _area to indicate they are just part of the location I guess
-  end,
+  end,--packs
   fovaSetupFunc=function(locationName,missionId)
     TppEneFova.SetupFovaForLocation("mafr")
     TppSoldierFace.SetUseZombieFova{enabled=true}
@@ -29,7 +30,22 @@ local this={
     -- TppSoldierFace.OverwriteMissionFovaData{body=body}
     -- TppSoldierFace.SetBodyFovaUserType{hostage={TppEnemyBodyId.prs6_main0_v00}}
     -- TppHostage2.SetDefaultBodyFovaId{parts="/Assets/tpp/parts/chara/prs/prs6_main0_def_v00.parts",bodyId=TppEnemyBodyId.prs6_main0_v00}
-  end,
-}
+  end,--fovaSetupFunc
+  missionMapParams={
+    --tex TODO: no actual heli support yet, but these work as start on foot
+    heliLandPoint={
+      {
+        point=Vector3(-71.862,0.584,248.143),
+        startPoint=Vector3(-71.862,0.584,248.143),
+        routeId="lz_drp_river_S|rt_drp_river_S",
+      },
+      {
+        point=Vector3(-57.259,2.371,-204.524),
+        startPoint=Vector3(-57.259,2.371,-204.524),
+        routeId="lz_drp_shack_N|rt_drp_shack_N",
+      },
+    },--heliLandPoint
+  },--missionMapParams
 
+}--this
 return this
