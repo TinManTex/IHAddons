aca5baf65306ecc75d296dc32b14dbaa508bd4b6
TinManTex <tinman.squad@gmail.com>
2021-07-27 12:00:14 +1200

    afn0

---
+++ tpp/locations/afn0/GameDir/mod/missions/33002_afn0.lua
@@ -0,0 +1,11 @@
+local this={
+	missionCode=33002,
+  location="AFN0",
+  packs={
+    "/Assets/tpp/pack/mission2/ih/quest_block.fpk",
+    "/Assets/tpp/pack/mission2/free/fafn0/fafn0_mission.fpk",
+  },
+  startPos={240.353,4.580,135.816},--rotY=-118.252,},
+}
+
+return this
