318d881ca6c68434e684b6aebdcacf0af6a3f778
TinManTex <tinman.squad@gmail.com>
2021-09-08 21:07:07 +1200

    location - mbi0

---
+++ tpp/locations/mbi0/GameDir/mod/locations/MBI0.lua
@@ -0,0 +1,7 @@
+local this={
+	description="MB Interior Example - Hangar",
+	locationName="MBI0",
+	locationId=301,
+	packs={"/Assets/tpp/pack/location/mbi0/mbi0.fpk"},
+}--this
+return this
---
+++ tpp/locations/mbi0/GameDir/mod/missions/34001_mbi0.lua
@@ -0,0 +1,22 @@
+local this={
+  missionCode=34001,
+  location="MBI0",
+  --hideMission=true,
+  --noAddVolunteers=true,
+  missionPacks={
+    --tex mbts interrogation hangar packs
+    "/Assets/tpp/pack/mission2/free/f30050/f30050_d090.fpk",--InterrogateQuiet
+    --"/Assets/tpp/pack/mission2/free/f30050/f30050_d120.fpk",--AnableDevBattleGear
+    --"/Assets/tpp/pack/mission2/free/f30050/f30050_d300.fpk",--DecisionHuey
+    --"/Assets/tpp/pack/mission2/free/f30050/f30050_d600.fpk",--TheGreatEscapeLiquid
+    --s10070_area4 - Hellbound
+    --s10140_area01 - METALLIC ARCHAEA
+    "/Assets/tpp/pack/mission2/free/f30050/f30050_ocelot.fpk",
+    "/Assets/tpp/pack/mission2/free/f30050/f30050_command_liquid.fpk",
+    "/Assets/tpp/pack/location/mbi0/pack_common/mtbs_hangar_patchup.fpk",--tex gate and door and some other guff from BG hangar to patch up interrogation hangar
+    "/Assets/tpp/pack/mission2/free/fmbi0/fmbi0_mission.fpk",
+    "/Assets/tpp/pack/mission2/ih/minimal_mission.fpk"
+  },--missionPacks
+  startPos={-11.600,-8.000,8.511},
+}--this
+return this
